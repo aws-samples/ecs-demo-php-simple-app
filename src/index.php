fca47e110ab4155214f0117090d063925343ea7d
cizal <ec2-user@ip-172-31-68-140.ec2.internal>
2018-01-30 23:14:38 +0000

    Testing the app

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Let's test this</h1>
+                <h1>Let's test this again</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
