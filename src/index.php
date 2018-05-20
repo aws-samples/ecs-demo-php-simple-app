58ae49af12c9bb577313ecf5f4718d798c1a5f1f
Jan Gnezda <jan@duuoo.io>
2018-05-20 11:14:34 +0200

    Papalinko

--- src/index.php
+++ src/index.php
@@ -15,6 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
+                <h1>PAPALINKO</h1>
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
