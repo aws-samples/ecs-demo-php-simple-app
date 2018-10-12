1e90e387427073f7223586a4d7e430bb0849720e
Apoorva Kulkarni <kuapoorv@amazon.com>
2018-10-12 13:58:40 -0700

    version 2

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
-                <h2>Congratulations V1 deployed</h2>
+                <h2>Congratulations V2 deployed</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
