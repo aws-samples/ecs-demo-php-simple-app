6dd283c3c7614f92647cce1b69ae847dc1db070d
JoviTelstra <33044720+JoviTelstra@users.noreply.github.com>
2019-05-30 08:16:07 +1000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,8 +15,8 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App-change 0757</h1>
-                <h2>Congratulations change 0804</h2>
+                <h1>Simple PHP App</h1>
+                <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
