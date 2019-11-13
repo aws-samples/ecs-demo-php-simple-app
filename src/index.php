b6047c5fabc789d1c5adfb09f2e6c56f7283c4e4
Nikunj Vaidya Macbook <vnikunj@amazon.com>
2019-11-13 00:37:56 -0800

    Changing BG color from Yellow to Orange
    Signed-off-by: Nikunj Vaidya Macbook <vnikunj@amazon.com>

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <!--[original bg color]<style>body {margin-top: 40px; background-color: #333;}</style>-->
-        <style>body {margin-top: 40px; background-color: #FFFF00;}</style>
+        <style>body {margin-top: 40px; background-color: #FFA500;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
