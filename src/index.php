5c2119cd97f485eb3c5532e1b8d70e20a2742026
MatheusSandre <matheus_sandre@hotmail.com>
2020-06-12 16:11:59 -0300

    UPdatre

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Matheus ECS PHP App</h1>
+                <h1>Matheus 2 ECS PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
